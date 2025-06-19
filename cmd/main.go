09651808d35aa9464d0f54a589b6695309fa10c2
nikolaihg <nikolai.gangstoe@gmail.com>
2025-06-19 23:35:47 +0200

    http and html parsing started

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 func main() {
-	name := "World"
-	fmt.Printf("Hello, %s!\n", name)
+	resp, err := http.Get("http://example.com")
+	if err != nil {
+		log.Fatalf("Error fetching the URL: %v", err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response status: ", resp.Status)
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		log.Fatalf("Error parsing html: %v", err)
+	}
+
+	html.Render(os.Stdout, doc)
+
+	traverseHTML(doc)
+}
+
+func traverseHTML(node *html.Node) {
+	if node.Type == html.ElementNode {
+		fmt.Println("Tag:", node.Data)
+	}
+	if node.Type == html.TextNode {
+		if node.Data != "" {
+			fmt.Print("Text:", strings.TrimSpace(node.Data))
+		}
+	}
+
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		traverseHTML(child)
+	}
 }
